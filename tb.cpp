897a2c7bb68bdfcda8a4535cd94f921cd152529b
Zeonus <sangphan0111@gmail.com>
2023-10-16 23:01:15 +0700

    stt 51 chinh sua file tb.cpp

---
+++ tb.cpp
@@ -0,0 +1,7 @@
+int main() {
+    std::cout << "Hello World!";
+    std::cout << "Hello World from number 51!";
+    std::cout << "Hello World from number 51!";
+    std::cout << "Hello World from number 51!";
+    return 0;
+}
