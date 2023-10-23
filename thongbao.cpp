e449e008c576620761d98fa7769a6acf54d861ed
Zeonus <sangphan0111@gmail.com>
2023-10-23 17:05:51 +0700

    main thay doi

--- thongbao.cpp
+++ thongbao.cpp
@@ -2,5 +2,7 @@ int main() {
     std::cout << "Hello World!";
     std::cout << "Hello World 123!";
 	std::cout << "Hello World so 52!";
+	std::cout << "Quang Sang - 52";
+	std::cout << "So 52 xin chao";
     return 0;
 }
